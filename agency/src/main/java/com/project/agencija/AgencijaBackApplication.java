2f2e6148bfadd0e8992f7a02c0bf04c6e2ac008b
Ivana Kasikovic <ikasikovic@yahoo.com>
2023-11-05 22:04:24 +0100

    Initialize projects

---
+++ agency/src/main/java/com/project/agencija/AgencijaBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.agencija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencijaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencijaBackApplication.class, args);
+	}
+
+}
---
+++ agency/src/test/java/com/project/agencija/AgencijaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.agencija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencijaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
