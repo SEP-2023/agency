d70a965db75bdf858c14b083b5322c2c87abac70
unknown <marijamaki26@gmail.com>
2023-11-11 16:13:23 +0100

    Add offers from agency

---
+++ agency-front/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {OffersComponent} from "./pages/offers/offers.component";
+
+const routes: Routes = [
+  { path: '', component: OffersComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- agency-front/src/app/app.module.ts
+++ agency-front/src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {RouterModule} from "@angular/router";
+import { OffersComponent } from './pages/offers/offers.component';
+import { AppRoutingModule } from './app-routing.module';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    OffersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    RouterModule,
+    AppRoutingModule,
+    MatGridListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
