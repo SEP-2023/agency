2809a973761deecc577d626fe79a8014b1f03ecf
Ivana Kasikovic <ikasikovic@yahoo.com>
2023-12-26 11:01:49 +0100

    Fix route

--- agency-front/src/app/app-routing.module.ts
+++ agency-front/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {OffersComponent} from "./pages/offers/offers.component";
 import {LoginRegistrationComponent} from "./pages/login-registration/login-registration.component";
 
 const routes: Routes = [
-  { path: '', component: LoginRegistrationComponent },
+  { path: '', component: OffersComponent },
   { path: 'offers', component: OffersComponent },
 
 ];
