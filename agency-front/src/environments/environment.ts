8804ce91874c7125f70b690b90f73bc67494db3b
Ivana Jankovic <ivanaj0610@gmail.com>
2024-01-30 18:57:42 +0100

    Extract host variable

--- agency-front/src/environments/environment.ts
+++ agency-front/src/environments/environment.ts
@@ -1,20 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+const host = '192.168.0.15'
 
 export const environment = {
   production: false,
-  // psp_service_url:"http://localhost:8083",
-  // agency_service_url:"http://localhost:8085",
-  agency_service_url:"http://192.168.0.15:8085",
-  psp_service_url:"http://192.168.0.15:8083"
+  agency_service_url: `http://${host}:8085`,
+  psp_service_url: `http://${host}:8083`
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
